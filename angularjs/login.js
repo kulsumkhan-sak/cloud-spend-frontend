c280e24041c1b1fe02c1b4fcf5b57eea270d17ef
kulsumkhan-sak <kulsumkhansak2@gmail.com>
2025-12-27 17:17:30 +0530

    Update login.js

--- angularjs/login.js
+++ angularjs/login.js
@@ -13,27 +13,26 @@ app.controller("LoginController", function ($scope, $http) {
     $scope.errorMessage = "";
 
     $http.post(
-  "https://cloudspend-api-hdc4d0f3e4h7cjdu.centralindia-01.azurewebsites.net/api/auth/login",
-  {
-    email: $scope.user.email,
-    password: $scope.user.password
-  }
-)
-
+      "http://localhost:5116/api/auth/login",
+      {
+        email: $scope.user.email,
+        password: $scope.user.password
+      },
+      {
+        withCredentials: true
+      }
+    )
     .then(function (response) {
-  console.log("API response:", response.data);
+      console.log("API response:", response.data);
 
-  $scope.$applyAsync(function () {
-    if (response.data.success) {
-      $scope.successMessage = response.data.message;
-    } else {
-      $scope.errorMessage = response.data.message;
-    }
-  });
-})
-
+      if (response.data.success) {
+        $scope.successMessage = response.data.message;
+      } else {
+        $scope.errorMessage = response.data.message;
+      }
+    })
     .catch(function (error) {
-      console.error(error);
+      console.error("Login error:", error);
       $scope.errorMessage = "Login failed";
     })
     .finally(function () {
